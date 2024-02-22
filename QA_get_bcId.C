bfd008061457cfd82c1b54831c92586d87ddfb49
Guillermo Contreras <jgcn@Guillermos-MacBook-Pro-2.local>
2024-02-22 11:40:00 +0100

    adding rest of the current macros

---
+++ QA_get_bcId.C
@@ -0,0 +1,22 @@
+#include "CandRoot.h"
+#include "GlobalVariables.h"
+#include "InputFromUser.h"
+#include "vdmUtilities.h"
+
+void QA_get_bcId(int fill)
+{
+    // get name of files and set pointers to trees
+  Set_input_file_names(fill);
+  Set_pointers_to_input_files_and_trees();
+
+  // get the ingo
+  int nIBC = GetNumberInteractingBunchCrossings();
+  int *bunches = new int [nIBC];
+  GetBunchIndices(bunches);
+
+  // print the info
+  cout << " There are " << nIBC << " interacting bunches" << endl;
+  for(int i=0;i<nIBC;i++) {
+    cout << "   bcID = " << bunches[i] << endl;
+  }
+}
