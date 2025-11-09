be7abc4bbb0a84c92b03d6544e83cdc599c2436f
Guillermo Contreras <jgcn@Guillermos-MacBook-Pro-7.local>
2025-11-09 09:03:54 +0100

    update InputFromUser.h

--- InputFromUser.h
+++ InputFromUser.h
@@ -9,12 +9,13 @@
 void Set_input_file_names(Int_t Fill)
 {
   //  	if (Fill == 8379) // pp @ 13.6 TeV Nov 10, 2022
-  //8379-> pp @ 13.6 TeV Nov 10 2022, 9128-> pp @ 13.6 TeV Sep 7-8 2023, 9644-> pp @ 13.6 TeV May 18 2024
+  // 8379-> pp @ 13.6 TeV Nov 10 2022, 9128-> pp @ 13.6 TeV Sep 7-8 2023, 9644-> pp @ 13.6 TeV May 18 2024
   // 10298-> pp @ 5.36 TeV, Oct-28-24
   // 10782 ->pO , 10802 -> OO, 10813 -> NeNe
   // 10813 -> pp 2025
-       	if (Fill == 8379 || Fill ==9128 || Fill ==9644 || Fill ==10298
-	    || Fill ==10782 || Fill ==10802 || Fill ==10813 || Fill ==10824)  
+  // 9240 -> PbPb 2023, 10338 -> PbPb 2024
+       	if (Fill == 8379 || Fill ==9128 || Fill ==9240 || Fill ==9644 || Fill ==10298
+	    || Fill ==10782 || Fill ==10802 || Fill ==10813 || Fill ==10824 || Fill == 10338)  
 	{
 		// set fill and number of scans in the fill
 		g_vdm_Fill = Fill;
@@ -32,6 +33,7 @@ void Set_input_file_names(Int_t Fill)
 		if (Fill == 10782) {gBeamB = 8;}
 		if (Fill == 10802) {gBeamA = gBeamB = 8;}
 		if (Fill == 10813) {gBeamA = gBeamB = 10;g_n_Scans_in_Fill = 1;}
+		if (Fill == 9240 || Fill == 10338) {gBeamA = gBeamB = 82;}
 				
 	}
 	else
